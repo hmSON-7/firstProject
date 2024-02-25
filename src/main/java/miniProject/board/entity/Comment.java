2f835060a96ac3d2d03a89b215f095b722d74a16
j4821_s <sonhm48021@gmail.com>
2024-02-25 14:05:52 +0900

    fix : Comment 연관 관계 문제

--- src/main/java/miniProject/board/entity/Comment.java
+++ src/main/java/miniProject/board/entity/Comment.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
-import miniProject.board.dto.CommentDto;
 
 @Entity
 @Getter
@@ -16,38 +14,6 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name="articleId")
-    private Article article;
-
-    @ManyToOne
-    @JoinColumn(name="memberId")
-    private Member member;
-
     @Column
     private String body;
-
-    public Comment createComment(CommentDto dto, Article article, Member member) {
-        if(dto.getId() != null)
-            throw new IllegalArgumentException("댓글 생성 실패! 댓글의 id가 없어야 합니다!");
-        if(!dto.getArticleId().equals(article.getId()))
-            throw new IllegalArgumentException("댓글 생성 실패! 게시글의 id가 잘못됐습니다!");
-
-        return new Comment(
-                dto.getId(),
-                article,
-                member,
-                dto.getBody()
-        );
-    }
-
-    public void updateComment(CommentDto dto) {
-        if(this.id.equals(dto.getId())) {
-            throw new IllegalArgumentException("댓글 수정 실패! 잘못된 id가 입력됐습니다!");
-        }
-
-        if(dto.getBody() != null) {
-            this.body = dto.getBody();
-        }
-    }
 }
