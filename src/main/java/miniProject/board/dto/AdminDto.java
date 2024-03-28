600f66d54b1a274711869204f06109bed3d12911
iju42829 <ijud4282@gmail.com>
2024-03-28 16:40:24 +0900

    feat : Admin 생성자 추가, AdminDto id 필드 추가

--- src/main/java/miniProject/board/dto/AdminDto.java
+++ src/main/java/miniProject/board/dto/AdminDto.java
@@ -1,16 +1,15 @@
 package miniProject.board.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import miniProject.board.entity.Admin;
 import miniProject.board.entity.Article;
 
+@Getter @Setter
 @AllArgsConstructor
 public class AdminDto {
-    private long id;
+    private Long id;
     private String name;
     private String password;
-
-    public Admin toEntity() {
-        return new Admin(id, name, password);
-    }
 }
