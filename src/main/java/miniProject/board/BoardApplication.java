1189e5564c6edfa8af499b42d17c1d61b6f4d130
iju42829 <ijud4282@gmail.com>
2024-07-16 16:22:09 +0900

    feat : BaseTimeEntity 및 JPA Auditing 설정 추가

--- src/main/java/miniProject/board/BoardApplication.java
+++ src/main/java/miniProject/board/BoardApplication.java
@@ -2,8 +2,9 @@ package miniProject.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BoardApplication {
 
