39a21e55cbc284b1d7dc103f35291e51326f798c
iju42829 <ijud4282@gmail.com>
2024-07-16 17:31:15 +0900

    docs : 주석 추가

--- src/main/java/miniProject/board/service/comment/CommentService.java
+++ src/main/java/miniProject/board/service/comment/CommentService.java
@@ -4,9 +4,62 @@ import miniProject.board.dto.CommentDto;
 
 import java.util.List;
 
+/**
+ * 댓글 관련 서비스 인터페이스입니다.
+ */
 public interface CommentService {
+
+    /**
+     * 멤버 ID와 게시글 ID를 기반으로 댓글을 생성합니다.
+     *
+     * @param memberId 댓글을 작성하는 멤버 Id
+     * @param articleId 댓글이 달릴 게시글의 ID
+     * @param createCommentRequest 댓글 내용
+     *
+     * @return 생성된 댓글의 ID
+     *
+     * @throws IllegalArgumentException 멤버 조회에 실패한 경우 또는 게시글 조회에 실패한 경우
+     */
     Long createComment(Long memberId, Long articleId, CommentDto.CreateRequest createCommentRequest);
+
+
+    /**
+     * 게시글 ID를 기반으로 해당 게시글에 달린 모든 댓글을 조회합니다.
+     *
+     * @param articleId 조회할 게시글 ID
+     *
+     * @return 게시글에 달린 모든 댓글 목록
+     *
+     * @throws IllegalArgumentException 게시글 조회에 실패한 경우
+     */
     List<CommentDto.Response> findCommentsByArticleId(Long articleId);
+
+
+    /**
+     * 댓글 ID와 멤버 ID를 기반으로 댓글을 삭제합니다.
+     *
+     * @param commentId 삭제할 댓글 ID
+     * @param memberId 멤버 ID
+     * @param deleteFrom 삭제 요청 페이지 구분 문자열
+     *
+     * @return 게시글에서 삭제 시 게시글 ID 반환, 멤버에서 삭제 시 멤버 ID 반환
+     *
+     * @throws IllegalArgumentException 댓글 조회에 실패한 경우
+     */
     Long deleteComment(Long commentId, Long memberId, String deleteFrom);
+
+
+    /**
+     * 댓글 ID와 멤버 ID를 기반으로 댓글을 수정합니다.
+     *
+     * @param commentId 수정할 댓글 ID
+     * @param memberId 멤버 ID
+     * @param updateFrom 업데이트 요청 페이지 구분 문자열
+     * @param updateCommentRequest 업데이트할 댓글의 내용
+     *
+     * @return 게시글에서 수정 시 게시글 ID 반환, 멤버에서 수정 시 멤버 ID 반환
+     *
+     * @throws IllegalArgumentException 댓글 조회에 실패한 경우
+     */
     Long updateComment(Long commentId, Long memberId, String updateFrom, CommentDto.UpdateRequest updateCommentRequest);
 }
