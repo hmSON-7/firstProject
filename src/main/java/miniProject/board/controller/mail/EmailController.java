db0d85ef772897add0f40412bf60b1a71db7984b
j4821_s <sonhm48021@gmail.com>
2024-08-06 17:06:32 +0900

    feat: 이메일 서비스 코드 작성중(미완)
    1. 이메일 전송시 난수 생성
    2. 비밀번효 변경에 요구되는 서비스 코드 프레임 작성

--- src/main/java/miniProject/board/controller/mail/EmailController.java
+++ src/main/java/miniProject/board/controller/mail/EmailController.java
@@ -1,8 +1,8 @@
 package miniProject.board.controller.mail;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import miniProject.board.service.mail.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class EmailController {
 
-    @Autowired
-    private EmailService emailService;
+    private final EmailService emailService;
 
     @GetMapping("/emailForm")
     public String showEmailForm(Model model) {
