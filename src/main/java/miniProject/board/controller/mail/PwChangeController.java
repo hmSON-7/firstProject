bacdc1b2664e090f35c2108d34c76ca1091a22be
j4821_s <sonhm48021@gmail.com>
2024-08-12 16:37:17 +0900

    fix: 일부 객체 변수명 변경

--- src/main/java/miniProject/board/controller/mail/PwChangeController.java
+++ src/main/java/miniProject/board/controller/mail/PwChangeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/changePW")
 public class PwChangeController {
 
-    private final PwChangeService PWChangeService;
+    private final PwChangeService pwChangeService;
 
     // 1. 이메일 인증 폼으로 이동
     @GetMapping("/emailCheck")
@@ -36,7 +36,7 @@ public class PwChangeController {
             return "redirect:/changePW/emailCheck";
         }
 
-        String code = PWChangeService.checkEmail(emailRequest.getUsername(), emailRequest.getEmail());
+        String code = pwChangeService.checkEmail(emailRequest.getUsername(), emailRequest.getEmail());
         log.debug("to: " + emailRequest.getEmail() + ", code: " + code);
 
         // 인증 폼에 필요한 데이터를 리다이렉트 속성에 추가
@@ -73,7 +73,7 @@ public class PwChangeController {
             return "redirect:/changePW/emailAuth";
         }
 
-        boolean check = PWChangeService.checkAuthNum(email, emailCheck.getAuthCode());
+        boolean check = pwChangeService.checkAuthNum(email, emailCheck.getAuthCode());
 
         if (!check) {
             log.debug("인증 번호 오류");
@@ -106,7 +106,7 @@ public class PwChangeController {
             return "redirect:/changePW/newPassword";
         }
 
-        boolean check = PWChangeService.changePW(changePWDto.getUsername(), changePWDto.getPassword());
+        boolean check = pwChangeService.changePW(changePWDto.getUsername(), changePWDto.getPassword());
         if (!check) {
             log.debug("비밀번호 변경 중 오류 발생");
             redirectAttributes.addFlashAttribute("username", changePWDto.getUsername());
