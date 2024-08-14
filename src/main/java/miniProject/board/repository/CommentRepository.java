7cbfdece331930471c253c591d80531ca0db5a23
iju42829 <ijud4282@gmail.com>
2024-08-14 15:18:19 +0900

    feat: 신고 기능을 위한 리포지토리 구현

--- src/main/java/miniProject/board/repository/CommentRepository.java
+++ src/main/java/miniProject/board/repository/CommentRepository.java
@@ -10,8 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Optional<Comment> findById(Long id);
-
     Optional<Comment> findByIdAndMemberId(Long commentId, Long memberId);
 
     Page<Comment> findByArticle(Article article, Pageable pageable);
