10fb28ab4a5d77846116ec26b6efa79ea6d85858
iju42829 <ijud4282@gmail.com>
2024-08-14 15:28:38 +0900

    feat: 신고 기능 구현을 위한 서비스 구현

---
+++ src/main/java/miniProject/board/service/report/ReportServiceImpl.java
@@ -0,0 +1,169 @@
+package miniProject.board.service.report;
+
+import lombok.RequiredArgsConstructor;
+import miniProject.board.dto.ReportDto;
+import miniProject.board.entity.*;
+import miniProject.board.entity.report.Report;
+import miniProject.board.entity.report.ReportArticle;
+import miniProject.board.entity.report.ReportComment;
+import miniProject.board.exception.ArticleNotFoundException;
+import miniProject.board.exception.CommentNotFoundException;
+import miniProject.board.exception.MemberNotFoundException;
+import miniProject.board.exception.report.MemberReportNotFoundException;
+import miniProject.board.exception.report.ReportNotFoundException;
+import miniProject.board.repository.ArticleRepository;
+import miniProject.board.repository.CommentRepository;
+import miniProject.board.repository.MemberRepository;
+import miniProject.board.repository.ReportRepository;
+import miniProject.board.service.ArticleService;
+import miniProject.board.service.constants.ReportAction;
+import miniProject.board.service.member.MemberSuspensionService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ReportServiceImpl implements ReportService {
+    private final MemberRepository memberRepository;
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
+    private final ReportRepository reportRepository;
+
+    private final ArticleService articleService;
+    private final MemberSuspensionService memberSuspensionService;
+
+    @Override
+    public void saveReportComment(Long memberId, Long commentId, ReportDto.Request commentReportRequest) {
+        Member member = memberRepository
+                .findById(memberId)
+                .orElseThrow(() ->
+                        new MemberNotFoundException("멤버가 존재하지 않습니다."));
+
+        Comment comment = commentRepository
+                .findById(commentId)
+                .orElseThrow(() ->
+                        new CommentNotFoundException("댓글이 존재하지 않습니다."));
+
+        ReportComment reportComment = ReportComment.createReportComment(commentReportRequest.getDescription(), comment, member);
+
+        reportRepository.save(reportComment);
+    }
+
+    @Override
+    public void saveReportArticle(Long memberId, Long articleId, ReportDto.Request articleReportRequest) {
+        Member member = memberRepository
+                .findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException("멤버가 존재하지 않습니다."));
+
+        Article article = articleRepository
+                .findById(articleId)
+                .orElseThrow(() -> new ArticleNotFoundException("게시글이 존재하지 않습니다."));
+
+        ReportArticle reportArticle = ReportArticle.createReportArticle(articleReportRequest.getDescription(), article, member);
+
+        reportRepository.save(reportArticle);
+    }
+
+    public List<ReportDto.CommentListResponse> getReportComments() {
+        return reportRepository
+                .findAllReportComments()
+                .stream()
+                .map(reportComment ->
+                        new ReportDto.CommentListResponse(reportComment.getId(),
+                                reportComment.getMember().getUsername(),
+                                reportComment.getDescription(),
+                                reportComment.getUpdatedAt()))
+                .toList();
+    }
+
+
+    public List<ReportDto.CommentResponse> getReportComment() {
+         return reportRepository
+                 .findAllReportComments()
+                 .stream()
+                 .map(reportComment ->
+                         new ReportDto.CommentResponse(reportComment.getMember().getUsername(),
+                                 reportComment.getDescription(),
+                                 reportComment.getUpdatedAt(),
+                                 reportComment.getComment().getMember().getUsername(),
+                                 reportComment.getComment().getContent(),
+                                 reportComment.getComment().getUpdatedAt()))
+                 .toList();
+    }
+
+    public List<ReportDto.ArticleListResponse> getReportArticles() {
+        return reportRepository.
+                findAllReportArticles()
+                .stream()
+                .map(reportArticle ->
+                        new ReportDto.ArticleListResponse(reportArticle.getId(),
+                                reportArticle.getMember().getUsername(),
+                                reportArticle.getDescription(),
+                                reportArticle.getUpdatedAt()))
+                .toList();
+    }
+
+    public List<ReportDto.ArticleResponse> getReportArticle() {
+        return reportRepository.
+                findAllReportArticles()
+                .stream()
+                .map(reportArticle ->
+                        new ReportDto.ArticleResponse(reportArticle.getMember().getUsername(),
+                                reportArticle.getDescription(),
+                                reportArticle.getArticle().getMember().getUsername(),
+                                articleService.read(reportArticle.getArticle().getArticleId()).getContent()))
+                .toList();
+    }
+
+    public void processReport(Long reportId,
+                              ReportAction reportAction,
+                              ReportDto.ProcessRequest processReportRequest) {
+
+        Report report = reportRepository
+                .findById(reportId)
+                .orElseThrow(() ->
+                        new ReportNotFoundException("신고를 찾을 수 없습니다."));
+
+        Long memberId;
+
+        if (report instanceof ReportArticle) {
+            memberId = ((ReportArticle) report).getArticle().getMember().getId();
+
+        } else if (report instanceof ReportComment) {
+            memberId = ((ReportComment) report).getComment().getMember().getId();
+
+        } else {
+            throw new IllegalStateException();
+        }
+
+        switch (reportAction) {
+            case BAN:
+                memberSuspensionService.applyPermanentSuspension(memberId);
+                report.updateStatus(ReportStatus.APPROVED);
+                break;
+
+            case SUSPEND:
+                memberSuspensionService.applyTemporarySuspension(memberId, processReportRequest.getDate());
+                report.updateStatus(ReportStatus.APPROVED);
+                break;
+
+            case REJECT:
+                report.updateStatus(ReportStatus.REJECTED);
+                break;
+
+            default:
+                throw new IllegalStateException();
+        }
+    }
+
+    public void delete(Long reportId, Long memberId) {
+        Report report = reportRepository
+                .findByIdAndMemberId(reportId, memberId)
+                .orElseThrow(MemberReportNotFoundException::new);
+
+        reportRepository.delete(report);
+    }
+}
