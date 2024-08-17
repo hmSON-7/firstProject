0453c93cca1db8fd7af12addda1ecd2bb47b9b01
iju42829 <ijud4282@gmail.com>
2024-08-17 14:35:41 +0900

    feat: 신고된 게시글 및 댓글 조회를 위한 쿼리 메서드 추가

--- src/main/java/miniProject/board/repository/ReportRepository.java
+++ src/main/java/miniProject/board/repository/ReportRepository.java
@@ -4,8 +4,10 @@ import miniProject.board.entity.Member;
 import miniProject.board.entity.report.Report;
 import miniProject.board.entity.report.ReportArticle;
 import miniProject.board.entity.report.ReportComment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,11 +15,19 @@ import java.util.Optional;
 public interface ReportRepository extends JpaRepository<Report, Long> {
     List<Report> findByMember(Member member);
 
-    @Query("select r from Report r where type(r) = ReportArticle")
+    @EntityGraph(attributePaths = {"member"})
+    @Query("select r from ReportArticle r")
     List<ReportArticle> findAllReportArticles();
 
-    @Query("select r from Report r where type(r) = ReportComment")
+    @EntityGraph(attributePaths = {"member"})
+    @Query("select r from ReportComment r")
     List<ReportComment> findAllReportComments();
 
     Optional<Report> findByIdAndMemberId(Long id, Long memberId);
+
+    @Query("select r from ReportArticle r where r.id = :id")
+    Optional<ReportArticle> findReportArticleById(@Param("id") Long id);
+
+    @Query("select r from ReportComment r where r.id = :id")
+    Optional<ReportComment> findReportCommentById(@Param("id") Long id);
 }
